9fac95601bb495ab933b3e3b3286b744a194de70
angelor88 <angelorussoniello88@gmail.com>
2018-02-23 16:00:06 -0800

    Add the main class to the project.

--- Models/Contacts.cs
+++ Models/Contacts.cs
@@ -1,3 +1,36 @@
 using System.Collections.Generic;
 using System;
+
 namespace MvcAddressBook.Models
+{
+  public class Contact
+  {
+    private string _name;
+    private static List<Contact> _instances = new List<Contact> {};
+
+    public Contact (string name)
+    {
+      _name = name;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public static List<Contact> GetAll()
+    {
+      return _instances;
+    }
+    public void Save()
+    {
+      _instances.Add(this);
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+}
